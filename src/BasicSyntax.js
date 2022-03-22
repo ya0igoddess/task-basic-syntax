caade0ff2d6274fa331aafcd3aba1042da93e59b
Daniil Genneke <genneke@mail.ru>
2022-03-22 22:14:54 +0300

    Система перевода

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,21 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
-    return result;
+    let num = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    let i = 0;
+    while (i < str.length - 1) {
+        if (num[str[i]] < num[str[i + 1]]) result -= num[str[i]];
+        else result += num[str[i]];
+        i++;
+    }
+
+    return result + num[str[str.length - 1]];
 }
